85f0a9d9490de6502732ec252c76d4a2631918ae
aszd456 <602419030@qq.com>
2021-03-18 01:58:37 +0800

    update

--- src/main/webapp/WEB-INF/statics/mybatisdemo.sql
+++ src/main/webapp/WEB-INF/statics/mybatisdemo.sql
@@ -1,22 +1,22 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : local
-Source Server Version : 50630
-Source Host           : localhost:3306
+Source Server         : localhost
+Source Server Version : 50711
+Source Host           : 127.0.0.1:3306
 Source Database       : mybatisdemo
 
 Target Server Type    : MYSQL
-Target Server Version : 50630
+Target Server Version : 50711
 File Encoding         : 65001
 
-Date: 2021-03-17 17:41:19
+Date: 2021-03-18 01:57:32
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for author
+-- Table structure for `author`
 -- ----------------------------
 DROP TABLE IF EXISTS `author`;
 CREATE TABLE `author` (
@@ -31,7 +31,7 @@ CREATE TABLE `author` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for book
+-- Table structure for `book`
 -- ----------------------------
 DROP TABLE IF EXISTS `book`;
 CREATE TABLE `book` (
@@ -49,7 +49,45 @@ CREATE TABLE `book` (
 -- ----------------------------
 
 -- ----------------------------
--- Table structure for tb_card
+-- Table structure for `role`
+-- ----------------------------
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  `nameZh` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of role
+-- ----------------------------
+INSERT INTO `role` VALUES ('1', 'dba', '数据库管理员');
+INSERT INTO `role` VALUES ('2', 'admin', '系统管理员');
+INSERT INTO `role` VALUES ('3', 'user', '用户');
+
+-- ----------------------------
+-- Table structure for `tb_article`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_article`;
+CREATE TABLE `tb_article` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(18) DEFAULT NULL,
+  `price` double(19,2) DEFAULT NULL,
+  `remark` varchar(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of tb_article
+-- ----------------------------
+INSERT INTO `tb_article` VALUES ('1', 'java', '30.00', 'java');
+INSERT INTO `tb_article` VALUES ('2', 'python', '25.00', 'python');
+INSERT INTO `tb_article` VALUES ('3', 'c#', '36.00', null);
+INSERT INTO `tb_article` VALUES ('4', 'c++', '60.00', null);
+
+-- ----------------------------
+-- Table structure for `tb_card`
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_card`;
 CREATE TABLE `tb_card` (
@@ -64,7 +102,7 @@ CREATE TABLE `tb_card` (
 INSERT INTO `tb_card` VALUES ('1', '123456');
 
 -- ----------------------------
--- Table structure for tb_clazz
+-- Table structure for `tb_clazz`
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_clazz`;
 CREATE TABLE `tb_clazz` (
@@ -80,7 +118,50 @@ CREATE TABLE `tb_clazz` (
 INSERT INTO `tb_clazz` VALUES ('1', 'j1601', 'java就业班');
 
 -- ----------------------------
--- Table structure for tb_person
+-- Table structure for `tb_item`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_item`;
+CREATE TABLE `tb_item` (
+  `order_id` int(11) NOT NULL,
+  `article_id` int(11) NOT NULL,
+  `amount` int(11) DEFAULT NULL,
+  PRIMARY KEY (`order_id`,`article_id`),
+  KEY `article_id` (`article_id`),
+  CONSTRAINT `tb_item_ibfk_1` FOREIGN KEY (`order_id`) REFERENCES `tb_order` (`id`),
+  CONSTRAINT `tb_item_ibfk_2` FOREIGN KEY (`article_id`) REFERENCES `tb_article` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of tb_item
+-- ----------------------------
+INSERT INTO `tb_item` VALUES ('1', '1', '1');
+INSERT INTO `tb_item` VALUES ('1', '2', '1');
+INSERT INTO `tb_item` VALUES ('1', '3', '2');
+INSERT INTO `tb_item` VALUES ('2', '1', '1');
+INSERT INTO `tb_item` VALUES ('2', '4', '2');
+
+-- ----------------------------
+-- Table structure for `tb_order`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_order`;
+CREATE TABLE `tb_order` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(32) DEFAULT NULL,
+  `total` double(19,2) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `tb_order_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of tb_order
+-- ----------------------------
+INSERT INTO `tb_order` VALUES ('1', '4ds5fsd46f54', '388.60', '1');
+INSERT INTO `tb_order` VALUES ('2', '654654sdfsdf', '217.80', '1');
+
+-- ----------------------------
+-- Table structure for `tb_person`
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_person`;
 CREATE TABLE `tb_person` (
@@ -100,7 +181,7 @@ CREATE TABLE `tb_person` (
 INSERT INTO `tb_person` VALUES ('1', 'jack', 'man', '23', '1');
 
 -- ----------------------------
--- Table structure for tb_student
+-- Table structure for `tb_student`
 -- ----------------------------
 DROP TABLE IF EXISTS `tb_student`;
 CREATE TABLE `tb_student` (
@@ -122,7 +203,26 @@ INSERT INTO `tb_student` VALUES ('2', '女', 'marry', '22', '1');
 INSERT INTO `tb_student` VALUES ('3', '男', 'tom', '24', '1');
 
 -- ----------------------------
--- Table structure for user
+-- Table structure for `tb_user`
+-- ----------------------------
+DROP TABLE IF EXISTS `tb_user`;
+CREATE TABLE `tb_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(18) DEFAULT NULL,
+  `login_name` varchar(18) DEFAULT NULL,
+  `password` varchar(18) DEFAULT NULL,
+  `phone` varchar(18) DEFAULT NULL,
+  `address` varchar(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of tb_user
+-- ----------------------------
+INSERT INTO `tb_user` VALUES ('1', '杰克', 'jack', '123456', '13565566984', '广州');
+
+-- ----------------------------
+-- Table structure for `user`
 -- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
@@ -142,3 +242,22 @@ INSERT INTO `user` VALUES ('3', '风气', '上海', '足球,篮球,乒乓球', n
 INSERT INTO `user` VALUES ('4', '风气', '上海', '足球,篮球,乒乓球', null);
 INSERT INTO `user` VALUES ('5', '风气', '上海', '足球,篮球,乒乓球', null);
 INSERT INTO `user` VALUES ('6', '风气', '上海', '足球,篮球,乒乓球', null);
+
+-- ----------------------------
+-- Table structure for `user_role`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_role`;
+CREATE TABLE `user_role` (
+  `id` int(11) NOT NULL,
+  `uid` int(11) DEFAULT NULL,
+  `rid` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of user_role
+-- ----------------------------
+INSERT INTO `user_role` VALUES ('1', '1', '1');
+INSERT INTO `user_role` VALUES ('2', '1', '2');
+INSERT INTO `user_role` VALUES ('3', '2', '2');
+INSERT INTO `user_role` VALUES ('4', '3', '3');
